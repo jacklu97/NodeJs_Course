61fc4ed1b9da2c040bbcfbe16d13a5e916551719
Luis Angel Hernández <jacklu.hdz@gmail.com>
2022-10-18 18:04:39 -0500

    Sending basic response

--- Understanding the basics/app.js
+++ Understanding the basics/app.js
@@ -10,7 +10,22 @@ const http = require('http');
 // But it is possible to use annonimous functions as well using arrow functions or traditional functions
 // createServer returns a server
 const server = http.createServer((req, res) => {
-    console.log(req)
+    console.log(req.url, req.method, req.headers)
+
+    // Response object doesn't contain too many information from scratch
+    // but it gets interesting when we add information
+
+    // We can set our headers as we need
+    res.setHeader('Content-Type', 'text/html');
+
+    // Response gets its body in chunks, this means piece by piece
+    res.write('<html>');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<body><h1>My first page</h1></body>');
+    res.write('</html>');
+
+    // When we are done, we use end method. If we try to write more data after it, we get an error
+    res.end();
 });
 
 // Keeps the server alive
