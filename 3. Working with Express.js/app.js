e94fb78ed53d7e8e9c30c38af8d6da2f3c630a32
Luis Angel Hernández <jacklu.hdz@gmail.com>
2022-11-28 14:11:14 -0600

    Serving static content using express middleware Added some basic style for html files Renamed 404 html file

--- 3. Working with Express.js/app.js
+++ 3. Working with Express.js/app.js
@@ -9,13 +9,14 @@ const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Adding a path on this use, matches all the paths starting with that pattern
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, '/views/page-not-found.html'));
+    res.status(404).sendFile(path.join(__dirname, '/views/404.html'));
 })
 
 // Keeps the server alive
