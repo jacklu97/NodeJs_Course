0fe114b096a071308e672a811dccd1f306a5167a
Luis Angel Hernández <jacklu.hdz@gmail.com>
2022-12-19 14:34:16 -0600

    Started next section Added dynamic route for product details

---
+++ 9. Dynamic Routes/routes/shop.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const express = require('express');
+
+const shopController = require('../controllers/shop');
+
+const router = express.Router();
+
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+// Dynamic routes must be declared at the end of the route so any static route can get triggered properly
+router.get('/products/:productId', shopController.getProduct);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
+
+module.exports = router;
