462a442985d78c629cf12b1c92ada7f1e2c548e2
Luis Angel Hernández <jacklu.hdz@gmail.com>
2022-12-21 17:59:52 -0600

    App allows user to delete items from cart Added bug fix where item is deleted from the cart if it is present

--- 9. Dynamic Routes/models/cart.js
+++ 9. Dynamic Routes/models/cart.js
@@ -44,6 +44,11 @@ module.exports = class Cart {
 
       const updatedCart = {...JSON.parse(fileContent)}
       const product = updatedCart.products.find(prod => prod.id === id);
+
+      if (!product) {
+        return;
+      }
+
       const productQty = product.qty;
       updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
       updatedCart.totalPrice -= Number(price) * productQty
